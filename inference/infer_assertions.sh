7383e8937324cbb746b3855d334cb98a9f86ffec
wojtas000 <56300703+wojtas000@users.noreply.github.com>
2023-01-19 19:56:01 +0100

    Update infer_assertions.sh

--- inference/infer_assertions.sh
+++ inference/infer_assertions.sh
@@ -2,12 +2,10 @@
 
 cp package/opencs.ttl.gz opencs2.ttl.gz;
 gzip -d opencs2.ttl.gz ;
-head -n50 opencs2.ttl;
 echo "extracted opencs";
 
 sed -i '/owl:imports <https:\/\/w3id.org\/ocs\/schema\/0.1.0>/a\    owl:imports <http://www.w3.org/2004/02/skos/core#> ;' opencs2.ttl ;
 sed -i '/owl:imports <https:\/\/w3id.org\/ocs\/schema\//d' opencs2.ttl;
-head -n50 opencs2.ttl;
 echo "deleted schema import, added skos import";
 
 java -jar robot.jar merge --input opencs/schema/schema.ttl --input opencs2.ttl --output output_opencs.ttl;
