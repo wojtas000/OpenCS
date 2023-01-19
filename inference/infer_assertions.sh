4db2e4e91b77e0bae70d7cd36079dfc1b8c1b622
wojtas000 <56300703+wojtas000@users.noreply.github.com>
2023-01-19 14:04:33 +0100

    Update infer_assertions.sh

--- inference/infer_assertions.sh
+++ inference/infer_assertions.sh
@@ -1,26 +1,7 @@
 #!/usr/bin bash
 
-# gzip -d `$HOME`/package/opencs.ttl.gz opencs.ttl
-# cp opencs.ttl opencs2.ttl
-
-# sed -i '/owl:imports <https:\/\/w3id.org\/ocs\/schema\//d' opencs2.ttl;
-
-# java -jar robot.jar merge --input package/opencs_schema.ttl --input opencs2.ttl --output output_opencs.ttl;
-
-# java -jar robot.jar remove --input output_opencs.ttl --axioms tbox --output output_opencs.ttl;
-
-# java -jar robot.jar remove --input output_opencs.ttl --select "annotation-properties data-properties anonymous classes" --output output_opencs.ttl;
-
-# java -jar robot.jar reason --reasoner HermiT --axiom-generators "PropertyAssertion" --input output_opencs.ttl --output output_opencs.ttl;
-
-# mv output_opencs.ttl output_files/inferred_opencs.ttl
-
-# # java -jar robot.jar diff --left output_opencs5.ttl --right output_opencs.ttl | grep '\+' | awk '{print substr($0, 3)}' > inferred_assertions.ttl;
-
-pwd;
-gzip -d package/opencs.ttl.gz ;
-ls;
-cp package/opencs.ttl opencs2.ttl ;
+cp package/opencs.ttl.gz opencs2.ttl.gz;
+gzip -d opencs2.ttl.gz ;
 head -n50 opencs2.ttl;
 echo "extracted opencs";
 
